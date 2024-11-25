517014c34479a733ec34ba2c9f7716d50c1896f0
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2024-11-25 10:12:37 +0700

    fix: add ;

--- assets/dompdf/index.php
+++ assets/dompdf/index.php
@@ -1,17 +1,19 @@
 <?php
+
 use Dompdf\Dompdf;
+
 require_once 'autoload.inc.php';
 
 // function pdf_create($html, $xfilename, $stream=true, $papersize = 'letter', $orientation = 'portrait')
 // {
 
-	// Reference the Dompdf namespace
-	
+// Reference the Dompdf namespace
 
-	// Instantiate and use the dompdf class
-	$dompdf = new Dompdf();
-	// Load HTML content
-	$html = "
+
+// Instantiate and use the dompdf class
+$dompdf = new Dompdf();
+// Load HTML content
+$html = "
 	<table>
 		<thead>
 			<tr>
@@ -23,16 +25,16 @@ require_once 'autoload.inc.php';
 				<td>data</td>
 			</tr>
 		</tbody>
-	</table>"
-	$dompdf->loadHtml('<h1>Welcome to CodexWorld.com</h1>');
+	</table>";
+$dompdf->loadHtml('<h1>Welcome to CodexWorld.com</h1>');
 
-	// (Optional) Setup the paper size and orientation
-	$dompdf->setPaper('A4', 'portait');
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'portait');
 
-	// Render the HTML as PDF
-	$dompdf->render();
-    ob_clean();
-    $dompdf->stream("laporan.pdf", array("Attachment" => FALSE));
+// Render the HTML as PDF
+$dompdf->render();
+ob_clean();
+$dompdf->stream("laporan.pdf", array("Attachment" => FALSE));
 
 
 // }
