8296d0c93a14d79e368952d25db7d4476c6a2c81
ramadhanabelio <ramadhanabelionusaputra@gmail.com>
2024-11-25 17:41:23 +0700

    docs: edit README.md file

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost/surat_disparbudpora/';
-$config['base_url'] = 'http://localhost:8080/surat_disparbudpora/';
+$config['base_url'] = 'http://localhost/surat_disparbudpora/';
+// $config['base_url'] = 'http://localhost:8080/surat_disparbudpora/';
 
 /*
 |--------------------------------------------------------------------------
